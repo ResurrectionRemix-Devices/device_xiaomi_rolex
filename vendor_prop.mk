f7107a62b353fe75053d485781ab3311b28bc120
KenHV <kenhv@null.net>
2020-09-29 14:14:57 +0000

    rolex:  prop: fix screen recorder lag

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -82,7 +82,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608 \
     ro.vendor.display.cabl=2 \
     ro.sf.lcd_density=295 \
-    vendor.display.enable_default_color_mode=1
+    vendor.display.enable_default_color_mode=1 \
+    debug.sdm.support_writeback=0 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.enable_hwc_vds=1
 
 # Fm
 PRODUCT_PROPERTY_OVERRIDES += \
